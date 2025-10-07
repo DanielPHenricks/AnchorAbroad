33fe741ace547a481ddaf858a6c790e088ce3259
Daniel Henricks <mrdanielhenricks@yahoo.com>
2025-10-07 10:22:36 -0500

    Hotfix

--- backend/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class Student(AbstractUser):
-    graduation_year = models.IntegerField(null=True, blank=True)
-    primary_major = models.CharField(max_length=255, null=True, blank=True)
-    
-    def __str__(self):
-        return self.username
