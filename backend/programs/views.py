31dbc9afc793950f8f2b65d0dbb5ef6bfb0cd89a
Daniel Henricks <mrdanielhenricks@yahoo.com>
2025-10-14 10:16:07 -0500

    Add the markers from the api

--- backend/programs/views.py
+++ backend/programs/views.py
@@ -1,12 +1,14 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Program
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import ProgramSerializer
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def list_programs(request): # List all the programs.
     programs = Program.objects.all()
     serializer = ProgramSerializer(programs, many=True)
-    return JsonResponse(serializer.data)
+    return JsonResponse(serializer.data, safe=False)
 
