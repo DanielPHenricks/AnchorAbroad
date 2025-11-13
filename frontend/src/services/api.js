02ea86de85f5a3794e564b8a6abe30cbd117ed3e
Daniel Henricks <mrdanielhenricks@yahoo.com>
2025-11-12 23:16:21 -0600

    Fix typo

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,7 @@
  */
 
 // This will need to be changed to the actual URL of the backend server when deploying.
-const API_BASE_URL =  process.env.API_BASE_URL || 'http://localhost:8000/api'
+const API_BASE_URL =  process.env.REACT_APP_API_URL || 'http://localhost:8000/api'
 
 class ApiService {
   constructor() {
